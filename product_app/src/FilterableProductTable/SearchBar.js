b0e7aa25ae10bd0d2bdefc6b1fb241b6d77576a0
atari-monk <atari.monk1@gmail.com>
2024-09-06 13:53:27 +0200

    state

--- product_app/src/FilterableProductTable/SearchBar.js
+++ product_app/src/FilterableProductTable/SearchBar.js
@@ -1,10 +1,11 @@
-export function SearchBar() {
-    return (
-        <form>
-            <input type="text" placeholder="Search..." />
-            <label>
-                <input type="checkbox" /> Only show products in stock
-            </label>
-        </form>
-    );
+export function SearchBar({ filterText, inStockOnly }) {
+  return (
+    <form>
+      <input type="text" value={filterText} placeholder="Search..." />
+      <label>
+        <input type="checkbox" checked={inStockOnly} /> Only show products in
+        stock
+      </label>
+    </form>
+  )
 }
